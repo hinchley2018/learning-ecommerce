14aa019168d79719fda782169e1a922baa995302
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2024-03-21 21:07:22 -0500

    lint

--- frontend/src/pages/products.js
+++ frontend/src/pages/products.js
@@ -1,32 +1,38 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from 'react-router';
 
 export default function Product() {
-  
   const navigate = useNavigate();
   const products = [
     {
       id: 1,
-      imageUrl: 'https://images.pexels.com/photos/585753/pexels-photo-585753.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageUrl:
+        'https://images.pexels.com/photos/585753/pexels-photo-585753.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       description: 'A cup fit for a software engineer.',
-      price: 4.99
-    }
+      price: 4.99,
+    },
   ];
 
   return (
-    <div data-testid="products">
-      <div className=" bg-gradient-to-r from-blue-800 via-blue-500 to-blue-800 p-5 text-center text-4xl">
-        <button
-          onClick={() => navigate("/")}
-          className="blue-500">
-            Home page
+    <div data-testid='products'>
+      <div className=' bg-gradient-to-r from-blue-800 via-blue-500 to-blue-800 p-5 text-center text-4xl'>
+        <button onClick={() => navigate('/')} className='blue-500'>
+          Home page
         </button>
       </div>
       <br></br>
-      <div class="p-6 bg-white rounded-b-xl" data-testid='products'>
-        {products.map(product =>(
+      <div class='p-6 bg-white rounded-b-xl' data-testid='products'>
+        {products.map((product) => (
           <div key={product.id}>
             <p>Product #: {product.id}</p>
-            <img src={product.imageUrl} alt={product.description} style={{ maxWidth: '100%', maxHeight: '150px', borderRadius: '10px' }}/>
+            <img
+              src={product.imageUrl}
+              alt={product.description}
+              style={{
+                maxWidth: '100%',
+                maxHeight: '150px',
+                borderRadius: '10px',
+              }}
+            />
             <p>{product.description}</p>
             <p>Price: ${product.price.toFixed(2)}</p>
           </div>
