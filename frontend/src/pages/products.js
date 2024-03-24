2384a41bac07dd22a2075d3e7669c79cbf5f718f
BHUSHAN <bhushannalawade318@gmail.com>
2024-03-24 12:22:45 +0530

    changes made

--- frontend/src/pages/products.js
+++ frontend/src/pages/products.js
@@ -1,46 +1,24 @@
-import { useEffect, useState } from 'react';
+
 import { useNavigate } from 'react-router';
-// import {useDispatch} from 'react-redux';
-// import { getallProducts } from '../action';
-import axios from 'axios';
-import { createClient } from 'pexels';
+
+
 
 export default function Product() {
   const navigate = useNavigate();
-  // const disatch = useDispatch();
-
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    // dispatch(getallProducts());
-    axios
-      .get('http://localhost:3001/products')
-      .then((res) => setList(res.data));
-  }, []);
-
-  let val = list.map((item) => {
-    return (
-      <ul>
-        <li key={item.id}>
-          <p>Product # :{item.id}</p>
-          <p>{item.description}</p>
-          <p>{item.price}</p>
-          <p>{item.quantity}</p>
-        </li>
-      </ul>
-    );
-  });
+  
 
   return (
     <div data-testid='products'>
       <div className=' bg-gradient-to-r from-blue-800 via-blue-500 to-blue-800 p-5 text-center text-4xl'>
         <button onClick={() => navigate('/')} className='blue-500'>
-          Home page
+          Home pagedsdssd
         </button>
       </div>
       <br></br>
       <div class='p-6 bg-white rounded-b-xl' data-testid='products'>
-        {val}
+          <button onClick={() => navigate('/products')} className='blue-500'>
+         Our Products
+        </button>
       </div>
     </div>
   );
--- frontend/src/pages/products/products.js
+++ frontend/src/pages/products/products.js
@@ -1,5 +1,9 @@
+import { useState,useEffect } from "react";
 import { Product } from "../../components/Product";
+import axios from 'axios';
 export default function ProductsPage() {
+  const [list, setList] = useState([]);
+  
   const products = [
     {
       id: 1,
@@ -9,7 +13,27 @@ export default function ProductsPage() {
       price: 4.99,
     },
   ];
+  useEffect(() => {
+    // dispatch(getallProducts());
+    axios
+      .get('http://localhost:3001/products')
+      .then((res) => setList(res.data));
+  }, []);
 
+
+  console.log(list);
+  let val = list.map((item) => {
+    return (
+      <ul>
+        <li key={item.id}>
+          <p>Product # :{item.id}</p>
+          <p>{item.description}</p>
+          <p>{item.price}</p>
+          <p>{item.quantity}</p>
+        </li>
+      </ul>
+    );
+  });
   return (
     <div data-testid="products-page">
       <div className="rounded-b-xl bg-white p-6" data-testid="products">
@@ -17,6 +41,7 @@ export default function ProductsPage() {
           <Product key={product.id} product={product} />
         ))}
       </div>
+      {val}
     </div>
   );
 }
