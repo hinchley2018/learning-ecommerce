0ae583918549408d4361c051a333b925b4e44071
Augustine Kyei <106200215+augustine24@users.noreply.github.com>
2024-04-08 13:41:42 -0400

    draft of button visual done
    just a draft of the button; i've gotten busy so haven't found time to figure out the way I should code the button out.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,12 @@
 import React, { useState } from 'react';
+export function Product({ product }) {
+  const [quantity, setQuantity] = useState(1);
 
-export function Product({ product, addToCart }) {
-  function ProductsPage() {
-    const [cart, setCart] = useState([]);
+  const buttonStyle = {
+    borderRadius: '10px',
+  };
 
-    const addToCart = (productId) => {
-      // Add the product to the cart based on productId
-      setCart([...cart, productId]);
-    };
-  }
-
-  import React, { useContext, useState } from 'react';
-
-  export function Product({ product }) {
-    const [quantity, setQuantity] = useState(1);
-
-    const buttonStyle = {
-      borderRadius: '10px',
-    };
-
-    /*
+  /*
   const handleAddToCart = () => {
     const cartItem = {
       id: product.id,
@@ -33,60 +20,49 @@ export function Product({ product, addToCart }) {
   };
   */
 
-    const handleChange = (e) => {
-      setQuantity(parseInt(e.target.value)); // Convert input value to integer
-    };
+  const handleChange = (e) => {
+    setQuantity(parseInt(e.target.value)); // Convert input value to integer
+  };
 
-    const { cart, setCart } = useContext(CartContext);
-
-    const handleAddToCart = () => {
-      setCart((prevCart) => [...prevCart, product]);
-    };
-
-    return (
-      <div key={product.id}>
-        <b>Product #: {product.id}</b>
-        <p>
-          <b>Name: {product.name}</b>
-        </p>
-        <img
-          src={product.imageUrl}
-          alt={product.description}
-          style={{
-            maxWidth: '100%',
-            maxHeight: '150px',
-            borderRadius: '10px',
-          }}
-        />
-        <p>{product.description}</p>
-        <b>
-          Price: ${product.price.toFixed(2)}
-          <br></br>
-        </b>
-        <b>Order quantity: </b>
-        <input
-          style={{ width: '40px' }}
-          type='number'
-          value={quantity}
-          onChange={handleChange}
-          min='1'
-          max={product.stock} // Set maximum quantity to available stock
-        />
+  return (
+    <div key={product.id}>
+      <b>Product #: {product.id}</b>
+      <p>
+        <b>Name: {product.name}</b>
+      </p>
+      <img
+        src={product.imageUrl}
+        alt={product.description}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '150px',
+          borderRadius: '10px',
+        }}
+      />
+      <p>{product.description}</p>
+      <b>
+        Price: ${product.price.toFixed(2)}
         <br></br>
-        <p
-          className='fas fa-cart-plus rounded-x1 mt-1 inline-block bg-blue-500 px-6 py-3 text-lg font-semibold text-yellow-400 hover:bg-blue-700'
-          style={buttonStyle}
-        >
-          <button>Add to Cart</button>
-        </p>
-        <p className='text-red-600'>
-          <b>Button not working yet</b>
-        </p>
-        <br></br>
-        <button className='rounded-xl bg-blue-600 p-5 text-yellow-400 hover:bg-blue-800'>
-          Add to Cart
-        </button>
-      </div>
-    );
-  }
+      </b>
+      <b>Order quantity: </b>
+      <input
+        style={{ width: '40px' }}
+        type='number'
+        value={quantity}
+        onChange={handleChange}
+        min='1'
+        max={product.stock} // Set maximum quantity to available stock
+      />
+      <br></br>
+      <p
+        className='fas fa-cart-plus rounded-x1 mt-1 inline-block bg-blue-500 px-6 py-3 text-lg font-semibold text-yellow-400 hover:bg-blue-700'
+        style={buttonStyle}
+      >
+        <button>Add to Cart</button>
+      </p>
+      <p className='text-red-600'>
+        <b>Button not working yet</b>
+      </p>
+    </div>
+  );
 }
--- frontend/src/pages/cart/cart.js
+++ frontend/src/pages/cart/cart.js
@@ -1,62 +1,51 @@
-<<<<<<< Updated upstream
 import React, { useState } from 'react';
-
 export default function Cart({ product, quantity }) {
   const [cart] = useState([]);
 
-
-   if (!product || !product.id) {
-     return <div className='bg-blue-800 p-5 text-xl text-yellow-400 '>
-       This is your cart!
-            </div>;
-      }
-    
-      return (
-        <div data-testid='cart'>
-          <div className='bg-blue-800 p-5 text-xl text-yellow-400 '>
-            This is your cart!
-          </div>
-            {Array.isArray(cart) && cart.map((product, index) => (
-            <div key={product.id}>
-              <b>Product #: {product.id}</b>
-              <p>
-                <b>Name: {product.name}</b>
-              </p>
-              <img
-                src={product.imageUrl}
-                alt={product.description}
-                style={{
-                  maxWidth: '100%',
-                  maxHeight: '150px',
-                  borderRadius: '10px',
-                }}
-              />
-              <p>{product.description}</p>
-              <b>
-                Price: ${product.price.toFixed(2)}
-                <br></br>
-              </b>
-              <b>Order quantity: </b>
-              <input
-                style={{ width: '40px' }}
-                type='number'
-                value={quantity}
-              />
-            </div>
-          ))}
-        </div>
-      );
-    }
-=======
-export default function Cart() {
+  if (!product || !product.id) {
+    return (
+      <div className='bg-blue-800 p-5 text-xl text-yellow-400 '>
+        This is your cart!
+      </div>
+    );
+  }
   return (
     <div data-testid='cart'>
       <div className='bg-blue-800 p-5 text-xl text-yellow-400 '>
         This is your cart!
       </div>
-      <h1>{cart.product.name}</h1>
-      <p>{cart.product.price}</p>
+      {Array.isArray(cart) &&
+        cart.map((product, index) => (
+          <div key={product.id}>
+            <b>Product #: {product.id}</b>
+            <p>
+              <b>Name: {product.name}</b>
+            </p>
+            <img
+              src={product.imageUrl}
+              alt={product.description}
+              style={{
+                maxWidth: '100%',
+                maxHeight: '150px',
+                borderRadius: '10px',
+              }}
+            />
+            <p>{product.description}</p>
+            <b>
+              Price: ${product.price.toFixed(2)}
+              <br></br>
+            </b>
+            <b>Order quantity: </b>
+            <input style={{ width: '40px' }} type='number' value={quantity} />
+            <div data-testid='cart'>
+              <div className='bg-blue-800 p-5 text-xl text-yellow-400 '>
+                This is your cart!
+              </div>
+              <h1>{cart.product.name}</h1>
+              <p>{cart.product.price}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
->>>>>>> Stashed changes
